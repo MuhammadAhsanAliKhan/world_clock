1c737f91d0d8695e92be35179a3a2520d25605e2
AhsanAli_0 <m.ahsan.ali21@gmail.com>
2023-03-11 15:28:13 +0500

    Basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-  print("hi");
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My first App'),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 173, 98, 48),
+    ),
+    body: Center(
+      child: Text(
+        'hello ninja',
+        style:  TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'DancingScript',
         ),
       ),
-    );
-  }
-}
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('click'),
+      backgroundColor: Color.fromARGB(255, 173, 98, 48),
+    ),
+  ),
+));
+
+
+
